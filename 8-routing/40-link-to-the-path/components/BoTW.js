1852473cc6ece29e971a4ce233a1c0cb38786386
DaPanda <156377255+DaPandamonium@users.noreply.github.com>
2024-10-22 00:50:07 +0300

    Small Misstypo
    Misstypo, unrelated letter after <p>

--- 8-routing/40-link-to-the-path/components/BoTW.js
+++ 8-routing/40-link-to-the-path/components/BoTW.js
@@ -2,7 +2,7 @@ export default function BoTW() {
     return (
       <div className="center">
         <img src="https://static.wikia.nocookie.net/the-legend-of-zelda-botw/images/c/c8/BotW_Link.png/revision/latest?cb=20200902181658" alt="Link Image" />
-        <p>yIn "The Legend of Zelda: Breath of the Wild" for the Nintendo Switch and Wii U, Link awakens from a century-long slumber in a ruined Hyrule. This Link must defeat Calamity Ganon and save Princess Zelda, exploring an expansive open world with unprecedented freedom. This incarnation of Link is more customizable than ever, with various outfits, weapons, and abilities to suit different playstyles. The game emphasizes exploration, survival, and a non-linear narrative, marking a significant evolution in the series. This Link is defined by his resilience and resourcefulness in a world filled with ancient technology and natural beauty.</p>
+        <p>In "The Legend of Zelda: Breath of the Wild" for the Nintendo Switch and Wii U, Link awakens from a century-long slumber in a ruined Hyrule. This Link must defeat Calamity Ganon and save Princess Zelda, exploring an expansive open world with unprecedented freedom. This incarnation of Link is more customizable than ever, with various outfits, weapons, and abilities to suit different playstyles. The game emphasizes exploration, survival, and a non-linear narrative, marking a significant evolution in the series. This Link is defined by his resilience and resourcefulness in a world filled with ancient technology and natural beauty.</p>
       </div>
     );
   }
