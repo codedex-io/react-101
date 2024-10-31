ba34b0f387f43904a680100032313b23c8ca486d
DaPanda <156377255+DaPandamonium@users.noreply.github.com>
2024-10-31 19:47:08 +0200

    Strict equality check & Updated img Links
    Strict equality check & Updated img Links for web response & URL interpretation and cross-platform consistency.

--- 1-first-react-app/04-building-blocks/LikeButton.js
+++ 1-first-react-app/04-building-blocks/LikeButton.js
@@ -3,18 +3,16 @@
 
 import { useState } from "react";
 
-const emptyHeartImage = "https://imgur.com/a/iNlNaPE";
-const fullHeartImage = "https://imgur.com/a/k6CdQmc";
+const emptyHeartImage = "https://i.imgur.com/wIq3Wbq.png";
+const fullHeartImage = "https://i.imgur.com/vyX9Vnx.png";
 
 export default function LikeButton() {
   const [likes, setLikes] = useState(0);
-
-  return (
-    <div className="like-info">
+	@@ -14,7 +14,7 @@ export default function LikeButton() {
       <div className="like-button" onClick={() => setLikes(likes + 1)}>
         {likes > 0 ? <img src={fullHeartImage} alt="Heart for like button" /> : <img src={emptyHeartImage} alt="Empty heart for like button" />}
       </div>
-      <p>{likes == 1 ? `  ${likes} Like` : `${likes} Likes`}</p>
+      <p>{likes === 1 ? `  ${likes} Like` : `${likes} Likes`}</p>
     </div>
   );
 }
